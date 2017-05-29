c6e5587f08216475a03c18c72e9cbed1d7475f24
villarleg <santialovera20@gmail.com>
2017-05-29 18:07:43 +0200

    Device: Remove low_latency permission
    It´s causing distorted sound

--- msm8996.mk
+++ msm8996.mk
@@ -29,7 +29,6 @@ PRODUCT_COPY_FILES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:system/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
