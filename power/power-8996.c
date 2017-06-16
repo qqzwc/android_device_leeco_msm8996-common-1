CyanogenMod/android_device_qcom_common

power: msm8996: Support boost and perf profile hints
Change-Id: I010d4d1d3fb495e523b065c11ca95c4305530f9f

power/power-8996.c
@@ -48,6 +48,60 @@
 #include "power-common.h"
 
 static int display_hint_sent;
+static int current_power_profile = PROFILE_BALANCED;
+
+extern void interaction(int duration, int num_args, int opt_list[]);
+
+static int profile_high_performance[14] = {
+    SCHED_BOOST_V3, 0x1,
+    CPUS_ONLINE_MIN_BIG, 0x2,
+    CPUS_ONLINE_MIN_LITTLE, 0x2,
+    MIN_FREQ_BIG_CORE_0, 0xFFF,
+    MIN_FREQ_BIG_CORE_1, 0xFFF,
+    MIN_FREQ_LITTLE_CORE_0, 0xFFF,
+    MIN_FREQ_LITTLE_CORE_1, 0xFFF,
+};
+
+static int profile_power_save[] = {
+    CPUS_ONLINE_MAX_BIG, 0x1,
+    CPUS_ONLINE_MAX_LITTLE, 0x2,
+    MAX_FREQ_BIG_CORE_0, 0x4B0,
+    MAX_FREQ_BIG_CORE_1, 0x4B0,
+    MAX_FREQ_LITTLE_CORE_0, 0x4B0,
+    MAX_FREQ_LITTLE_CORE_1, 0x4B0,
+};
+
+int get_number_of_profiles() {
+    return 3;
+}
+
+static void set_power_profile(int profile) {
+
+    if (profile == current_power_profile)
+        return;
+
+    ALOGV("%s: profile=%d", __func__, profile);
+
+    if (current_power_profile != PROFILE_BALANCED) {
+        undo_hint_action(DEFAULT_PROFILE_HINT_ID);
+        ALOGV("%s: hint undone", __func__);
+    }
+
+    if (profile == PROFILE_HIGH_PERFORMANCE) {
+        int *resource_values = profile_high_performance;
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, resource_values,
+                ARRAY_SIZE(resource_values));
+        ALOGD("%s: set performance mode", __func__);
+
+    } else if (profile == PROFILE_POWER_SAVE) {
+        int *resource_values = profile_power_save;
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, resource_values,
+                ARRAY_SIZE(resource_values));
+        ALOGD("%s: set powersave", __func__);
+    }
+
+    current_power_profile = profile;
+}
 
 static int process_cam_preview_hint(void *metadata)
 {
@@ -91,9 +145,18 @@ static int process_cam_preview_hint(void *metadata)
              *  low power ceil mpbs - 2500
              *  low power io percent - 50
              */
-            int resource_values[] = {0x41400000, 0x4, 0x41410000, 0x5F, 0x41414000, 0x22C,
-                0x41420000, 0x5A, 0x41400100, 0x4, 0x41410100, 0x5F, 0x41414100, 0x22C,
-                0x41420100, 0x5A, 0x41810000, 0x9C4, 0x41814000, 0x32};
+            int resource_values[] = {
+                ABOVE_HISPEED_DELAY_BIG, 0x4,
+                GO_HISPEED_LOAD_BIG, 0x5F,
+                HISPEED_FREQ_BIG, 0x22C,
+                TARGET_LOADS_BIG, 0x5A,
+                ABOVE_HISPEED_DELAY_LITTLE, 0x4,
+                GO_HISPEED_LOAD_LITTLE, 0x5F,
+                HISPEED_FREQ_LITTLE, 0x22C,
+                TARGET_LOADS_LITTLE, 0x5A,
+                LOW_POWER_CEIL_MBPS, 0x9C4,
+                LOW_POWER_IO_PERCENT, 0x32
+            };
 
             perform_hint_action(cam_preview_metadata.hint_id,
                     resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
@@ -156,12 +219,23 @@ static int process_video_encode_hint(void *metadata)
              *    -bus dcvs hysteresis tuning
              *    -sample_ms of 10 ms
              *    -disable ignore_hispeed_notif
-             *    -sLVT hispeed freq to 806MHz
              */
-            int resource_values[] = {0x41400000, 0x4, 0x41410000, 0x5F, 0x41414000, 0x326,
-                0x41420000, 0x5A, 0x41400100, 0x4, 0x41410100, 0x5F, 0x41414100, 0x22C, 0x41420100, 0x5A,
-                0x41810000, 0x9C4, 0x41814000, 0x32, 0x4180C000, 0x0, 0x41820000, 0xA,
-                0x41438100, 0x0,  0x41438000, 0x0};
+            int resource_values[] =
+                ABOVE_HISPEED_DELAY_BIG, 0x4,
+                GO_HISPEED_LOAD_BIG, 0x5F,
+                HISPEED_FREQ_BIG, 0x326,
+                TARGET_LOADS_BIG, 0x5A,
+                ABOVE_HISPEED_DELAY_LITTLE, 0x4,
+                GO_HISPEED_LOAD_LITTLE, 0x5F,
+                HISPEED_FREQ_LITTLE, 0x22C,
+                TARGET_LOADS_LITTLE, 0x5A,
+                LOW_POWER_CEIL_MBPS, 0x9C4,
+                LOW_POWER_IO_PERCENT, 0x32,
+                CPUBW_HWMON_V1, 0x0
+                CPUBW_HWMON_SAMPLE_MS, 0xA,
+                IGNORE_HISPEED_NOTIF_LITTLE, 0x0,
+                IGNORE_HISPEED_NOTIF_BIG, 0x0
+            };
 
             perform_hint_action(video_encode_metadata.hint_id,
                     resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
@@ -181,18 +255,68 @@ static int process_video_encode_hint(void *metadata)
 
 int power_hint_override(struct power_module *module, power_hint_t hint, void *data)
 {
-    int ret_val = HINT_NONE;
-    switch(hint) {
-        case POWER_HINT_CAM_PREVIEW:
-            ret_val = process_cam_preview_hint(data);
-            break;
-        case POWER_HINT_VIDEO_ENCODE:
-            ret_val = process_video_encode_hint(data);
-            break;
-        default:
-            break;
+    int duration;
+    int resources_launch_boost[] = {
+        SCHED_BOOST_V3, 0x1,
+        MAX_FREQ_BIG_CORE_0, 0xFFF,
+        MAX_FREQ_LITTLE_CORE_0, 0xFFF,
+        MIN_FREQ_BIG_CORE_0, 0xFFF,
+        MIN_FREQ_LITTLE_CORE_0, 0xFFF,
+        CPUBW_HWMON_MIN_FREQ, 0x8C,
+        PWR_CLPS_V3, 0x1
+        STORAGE_CLK_SCALE_DIS, 0x1
+    };
+    int resources_cpu_boost[] = {
+        CPUBW_HWMON_MIN_FREQ, 0x33,
+        MIN_FREQ_BIG_CORE_0, 0x3E8,
+        MIN_FREQ_LITTLE_CORE_0, 0x3E8,
+        SCHED_BOOST_V3, 0x1,
+        SCHED_GROUP, 0x1
+    };
+
+    if (hint == POWER_HINT_SET_PROFILE) {
+        set_power_profile(*(int32_t *)data);
+        return HINT_HANDLED;
     }
-    return ret_val;
+
+    /* Skip other hints in custom power modes */
+    if (current_power_profile != PROFILE_BALANCED) {
+        return HINT_HANDLED;
+    }
+
+    if (hint == POWER_HINT_LAUNCH_BOOST) {
+        launch_boost_info_t *info = (launch_boost_info_t *)data;
+        if (info == NULL) {
+            ALOGE("Invalid argument for launch boost");
+            return HINT_HANDLED;
+        }
+
+        duration = 2000;
+
+        ALOGV("LAUNCH_BOOST: %s (pid=%d)", info->packageName, info->pid);
+
+        start_prefetch(info->pid, info->packageName);
+        interaction(duration, ARRAY_SIZE(resources_launch_boost),
+                resources_launch_boost);
+        return HINT_HANDLED;
+    }
+
+    if (hint == POWER_HINT_CPU_BOOST) {
+        duration = *(int32_t *)data / 1000;
+        if (duration > 0) {
+            interaction(duration, ARRAY_SIZE(resources_cpu_boost),
+                    resources_cpu_boost);
+            return HINT_HANDLED;
+        }
+    }
+
+    if (hint == POWER_HINT_CAM_PREVIEW)
+        return process_cam_preview_hint(data);
+
+    if (hint == POWER_HINT_VIDEO_ENCODE)
+        return process_video_encode_hint(data);
+
+    return HINT_NONE;
 }
 
 int set_interactive_override(struct power_module *module, int on)

qqzwc/android_device_leeco_msm8996-common-1

power/power-8996.c
@@ -51,6 +51,81 @@ static int display_hint_sent;
 int launch_handle = -1;
 int launch_mode;
 
+static int current_power_profile = PROFILE_BALANCED;
+
+extern void interaction(int duration, int num_args, int opt_list[]);
+
+int get_number_of_profiles() {
+    return 5;
+}
+
+static int profile_high_performance[] = {
+    SCHED_BOOST_ON_V3, 0x1,
+    ALL_CPUS_PWR_CLPS_DIS_V3, 0x1,
+    CPUS_ONLINE_MIN_BIG, 0x2,
+    CPUS_ONLINE_MIN_LITTLE, 0x2,
+    MIN_FREQ_BIG_CORE_0, 0xFFF,
+    MIN_FREQ_LITTLE_CORE_0, 0xFFF,
+};
+
+static int profile_power_save[] = {
+    CPUS_ONLINE_MAX_LIMIT_BIG, 0x1,
+    MAX_FREQ_BIG_CORE_0, 0x3E8,
+    MAX_FREQ_LITTLE_CORE_0, 0x3E8,
+};
+
+static int profile_bias_power[] = {
+    MAX_FREQ_BIG_CORE_0, 0x514,
+    MAX_FREQ_LITTLE_CORE_0, 0x3E8,
+};
+
+static int profile_bias_performance[] = {
+    CPUS_ONLINE_MAX_LIMIT_BIG, 0x2,
+    CPUS_ONLINE_MAX_LIMIT_LITTLE, 0x2,
+    MIN_FREQ_BIG_CORE_0, 0x578,
+};
+
+static void set_power_profile(int profile) {
+
+    if (profile == current_power_profile)
+        return;
+
+    ALOGV("%s: Profile=%d", __func__, profile);
+
+    if (current_power_profile != PROFILE_BALANCED) {
+        undo_hint_action(DEFAULT_PROFILE_HINT_ID);
+        ALOGV("%s: Hint undone", __func__);
+    }
+
+    if (profile == PROFILE_POWER_SAVE) {
+        int *resource_values = profile_power_save;
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, resource_values,
+                ARRAY_SIZE(resource_values));
+        ALOGD("%s: Set powersave mode", __func__);
+
+    } else if (profile == PROFILE_HIGH_PERFORMANCE) {
+        int *resource_values = profile_high_performance;
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, resource_values,
+                ARRAY_SIZE(resource_values));
+        ALOGD("%s: Set performance mode", __func__);
+
+    } else if (profile == PROFILE_BIAS_POWER) {
+        int *resource_values = profile_bias_power;
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, resource_values,
+                ARRAY_SIZE(resource_values));
+        ALOGD("%s: Set bias power mode", __func__);
+
+    } else if (profile == PROFILE_BIAS_PERFORMANCE) {
+        int *resource_values = profile_bias_performance;
+        perform_hint_action(DEFAULT_PROFILE_HINT_ID, resource_values,
+                ARRAY_SIZE(resource_values));
+        ALOGD("%s: Set bias perf mode", __func__);
+
+    }
+
+    current_power_profile = profile;
+}
+
 #ifdef EXTRA_POWERHAL_HINTS
 static int process_cam_preview_hint(void *metadata)
 {
@@ -94,12 +169,21 @@ static int process_cam_preview_hint(void *metadata)
              *  low power ceil mpbs - 2500
              *  low power io percent - 50
              */
-            int resource_values[] = {0x41400000, 0x4, 0x41410000, 0x5F, 0x41414000, 0x22C,
-                0x41420000, 0x5A, 0x41400100, 0x4, 0x41410100, 0x5F, 0x41414100, 0x22C,
-                0x41420100, 0x5A, 0x41810000, 0x9C4, 0x41814000, 0x32};
+            int resource_values[] = {
+                ABOVE_HISPEED_DELAY_BIG, 0x4,
+                GO_HISPEED_LOAD_BIG, 0x5F,
+                HISPEED_FREQ_BIG, 0x22C,
+                TARGET_LOADS_BIG, 0x5A,
+                ABOVE_HISPEED_DELAY_LITTLE, 0x4,
+                GO_HISPEED_LOAD_LITTLE, 0x5F,
+                HISPEED_FREQ_LITTLE, 0x22C,
+                TARGET_LOADS_LITTLE, 0x5A,
+                LOW_POWER_CEIL_MBPS, 0x9C4,
+                LOW_POWER_IO_PERCENT, 0x32,
+            };
 
             perform_hint_action(cam_preview_metadata.hint_id,
-                    resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+                    resource_values, ARRAY_SIZE(resource_values));
             ALOGI("Cam Preview hint start");
             return HINT_HANDLED;
         } else if ((strncmp(governor, SCHED_GOVERNOR, strlen(SCHED_GOVERNOR)) == 0) &&
@@ -210,14 +294,26 @@ static int process_video_encode_hint(void *metadata)
              *    -bus dcvs hysteresis tuning
              *    -sample_ms of 10 ms
              *    -disable ignore_hispeed_notif
-             *    -sLVT hispeed freq to 806MHz
              */
             int resource_values[] = {
-                0x41810000, 0x9C4, 0x41814000, 0x32, 0x4180C000, 0x0, 0x41820000, 0xA,
-                0x41438100, 0x1,  0x41438000, 0x1 };
+                ABOVE_HISPEED_DELAY_BIG, 0x4,
+                GO_HISPEED_LOAD_BIG, 0x5F,
+                HISPEED_FREQ_BIG, 0x326,
+                TARGET_LOADS_BIG, 0x5A,
+                ABOVE_HISPEED_DELAY_LITTLE, 0x4,
+                GO_HISPEED_LOAD_LITTLE, 0x5F,
+                HISPEED_FREQ_LITTLE, 0x22C,
+                TARGET_LOADS_LITTLE, 0x5A,
+                LOW_POWER_CEIL_MBPS, 0x9C4,
+                LOW_POWER_IO_PERCENT, 0x32,
+                CPUBW_HWMON_V1, 0x0,
+                CPUBW_HWMON_SAMPLE_MS, 0xA,
+                IGNORE_HISPEED_NOTIF_LITTLE, 0x0,
+                IGNORE_HISPEED_NOTIF_BIG, 0x0,
+            };
 
             perform_hint_action(video_encode_metadata.hint_id,
-                    resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+                    resource_values, ARRAY_SIZE(resource_values));
             ALOGI("Video Encode hint start");
             return HINT_HANDLED;
         } else if ((strncmp(governor, SCHED_GOVERNOR, strlen(SCHED_GOVERNOR)) == 0) &&
@@ -234,7 +330,7 @@ static int process_video_encode_hint(void *metadata)
                                      0x4180C000, 0x0,   0x41820000, 0xA};
 
             perform_hint_action(video_encode_metadata.hint_id,
-                    resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+                    resource_values, ARRAY_SIZE(resource_values));
             ALOGI("Video Encode hint start");
             return HINT_HANDLED;
         }
@@ -280,26 +376,106 @@ static int process_activity_launch_hint(void *data)
 
 int power_hint_override(struct power_module *module, power_hint_t hint, void *data)
 {
-    int ret_val = HINT_NONE;
-    switch(hint) {
-#ifdef EXTRA_POWERHAL_HINTS
-        case POWER_HINT_CAM_PREVIEW:
-            ret_val = process_cam_preview_hint(data);
-            break;
-#endif
-        case POWER_HINT_VIDEO_ENCODE:
-            ret_val = process_video_encode_hint(data);
-            break;
-        case POWER_HINT_LAUNCH:
-            ret_val = process_activity_launch_hint(data);
-            break;
-        default:
-            break;
+    struct timeval cur_boost_timeval = {0, 0};
+    static unsigned long long previous_boost_time = 0;
+    unsigned long long cur_boost_time;
+    double elapsed_time;
+    int duration;
+
+    int resources_launch[] = {
+        SCHED_BOOST_ON_V3, 0x1,
+        MAX_FREQ_BIG_CORE_0, 0xFFF,
+        MAX_FREQ_LITTLE_CORE_0, 0xFFF,
+        MIN_FREQ_BIG_CORE_0, 0xFFF,
+        MIN_FREQ_LITTLE_CORE_0, 0xFFF,
+        CPUBW_HWMON_MIN_FREQ, 0x8C,
+        ALL_CPUS_PWR_CLPS_DIS_V3, 0x1,
+        STOR_CLK_SCALE_DIS, 0x1,
+    };
+
+    int resources_cpu_boost[] = {
+        SCHED_BOOST_ON_V3, 0x1,
+        MIN_FREQ_BIG_CORE_0, 0x3E8,
+    };
+
+    int resources_interaction_fling_boost[] = {
+        CPUBW_HWMON_MIN_FREQ, 0x33,
+        MIN_FREQ_BIG_CORE_0, 0x3E8,
+        MIN_FREQ_LITTLE_CORE_0, 0x3E8,
+        SCHED_BOOST_ON_V3, 0x1,
+        SCHED_GROUP_ON, 0x1,
+    };
+
+    int resources_interaction_boost[] = {
+        MIN_FREQ_BIG_CORE_0, 0x3E8,
+    };
+
+    if (hint == POWER_HINT_SET_PROFILE) {
+        set_power_profile(*(int32_t *)data);
+        return HINT_HANDLED;
     }
-    return ret_val;
+
+    /* Skip other hints in power save mode */
+    if (current_power_profile == PROFILE_POWER_SAVE)
+        return HINT_HANDLED;
+
+    if (hint == POWER_HINT_INTERACTION) {
+        duration = data ? *((int *)data) : 500;
+
+        gettimeofday(&cur_boost_timeval, NULL);
+        cur_boost_time = cur_boost_timeval.tv_sec * 1000000 + cur_boost_timeval.tv_usec;
+        elapsed_time = (double) (cur_boost_time - previous_boost_time);
+        if (elapsed_time > 750000)
+            elapsed_time = 750000;
+        /**
+         * Don't hint if it's been less than 250ms since last boost
+         * also detect if we're doing anything resembling a fling
+         * support additional boosting in case of flings
+         */
+        else if (elapsed_time < 250000 && duration <= 750)
+            return HINT_HANDLED;
+
+        previous_boost_time = cur_boost_time;
+
+        if (duration >= 1500) {
+            interaction(duration, ARRAY_SIZE(resources_interaction_fling_boost),
+                    resources_interaction_fling_boost);
+        } else {
+            interaction(duration, ARRAY_SIZE(resources_interaction_boost),
+                    resources_interaction_boost);
+        }
+        return HINT_HANDLED;
+    }
+
+    if (hint == POWER_HINT_LAUNCH) {
+        duration = 2000;
+
+        interaction(duration, ARRAY_SIZE(resources_launch),
+                resources_launch);
+        return HINT_HANDLED;
+    }
+
+    if (hint == POWER_HINT_CPU_BOOST) {
+        duration = *(int32_t *)data / 1000;
+        if (duration > 0) {
+            interaction(duration, ARRAY_SIZE(resources_cpu_boost),
+                    resources_cpu_boost);
+            return HINT_HANDLED;
+        }
+    }
+
+#ifdef EXTRA_POWERHAL_HINTS
+    if (hint == POWER_HINT_CAM_PREVIEW)
+        return process_cam_preview_hint(data);
+#endif
+
+    if (hint == POWER_HINT_VIDEO_ENCODE)
+        return process_video_encode_hint(data);
+
+    return HINT_NONE;
 }
 
-int set_interactive_override(struct power_module *module, int on)
+int set_interactive_override(__unused struct power_module *module, int on)
 {
     return HINT_HANDLED; /* Don't excecute this code path, not in use */
     char governor[80];
@@ -317,7 +493,7 @@ int set_interactive_override(struct power_module *module, int on)
             int resource_values[] = {}; /* dummy node */
             if (!display_hint_sent) {
                 perform_hint_action(DISPLAY_STATE_HINT_ID,
-                resource_values, sizeof(resource_values)/sizeof(resource_values[0]));
+                resource_values, ARRAY_SIZE(resource_values));
                 display_hint_sent = 1;
                 ALOGI("Display Off hint start");
                 return HINT_HANDLED;